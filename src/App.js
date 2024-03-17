0aac606a84d2bf7b7ccf2d878aa4d74d620d6162
DevEmman <eolasunkanmi@gmail.com>
2024-03-17 22:17:40 +0000

    created a project with react app and added files for homepage

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+
+import HomePage from './homepage.component';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">Hello World,My name is Emmanuel this is my 1st time,working with react</header>
+      <HomePage />
+    </div>
+  );
+}
+
+export default App;
