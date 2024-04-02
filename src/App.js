8b8d0750b2bf225cd60e01b7b3c91b37cd99efb2
DevEmman <eolasunkanmi@gmail.com>
2024-04-02 10:53:14 +0000

    update our menuItems to navigate HOC

--- src/App.js
+++ src/App.js
@@ -1,14 +1,45 @@
 import React from 'react';
+
+import { Route, Routes } from 'react-router-dom';
+
 import './App.css';
 
 import HomePage from './component/pages/homepage/homepage.component';
 
+const HatsPage = () => (
+  <div>
+    <h1>HATS PAGE</h1>
+  </div>
+);
+
+const ShowsPage = () => (
+  <div>
+    <h2>Show Room</h2>
+  </div>
+);
+
+const sneakers = () => (
+  <div>
+    <h2>sneakers</h2>
+  </div>
+);
+
+const women = () => (
+  <div>
+    <h2>Woman wears</h2>
+  </div>
+);
+
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">Hello World,My name is Emmanuel this is my 1st time,working with react</header>
-      <HomePage />
-    </div>
+    <Routes>
+        <Route path='/' Component={HomePage}/>
+        <Route path='/shop/hats' Component={HatsPage}/> 
+        <Route path='/shop/jackets' Component={ShowsPage}/>
+        <Route path='/shop/sneakers' Component={sneakers}/>  
+        <Route path='/shop/womens' Component={women}/>
+    </Routes>
+            
   );
 }
 
--- src/component/menu-item/menu-item.component.jsx
+++ src/component/menu-item/menu-item.component.jsx
@@ -1,19 +1,25 @@
 import React from "react";
+import { useNavigate } from 'react-router-dom';
 
 import '../menu-item/menu-item.style.css';
 
-const MenuItem = ({ title,imageUrl,size }) => (
-    <div className={`${size} menu-item`} >
-       <div 
-         className="background-image" 
-         style={{ backgroundImage: `url( ${imageUrl} )` 
-          }}/>
+const MenuItem = ({ title, imageUrl, size, history, linkUrl}) => {
+    const navigate = useNavigate();
 
+  return (
+    <div className={`${size} menu-item`}
+      onClick={() => navigate(linkUrl)}>
+        <div 
+          className="background-image" 
+          style={{ backgroundImage: `url( ${imageUrl} )` 
+          }}/>
+  
         <div className="content">
             <h1 className="title">{ title.toUpperCase() }</h1>
-               <span className="subtitle">Shop Now</span>
+            <span className="subtitle">Shop Now</span>
             </div>
         </div> 
-);
+      )
+    };
 
 export default MenuItem;
