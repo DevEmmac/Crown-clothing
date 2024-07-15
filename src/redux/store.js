4c7782af040d99ca709b5dda8d72cc183799df5f
DevEmman <eolasunkanmi@gmail.com>
2024-07-15 16:08:01 +0000

    updating our app to redirect to home if user is user is signed in

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,24 +1,18 @@
 import { configureStore } from "@reduxjs/toolkit";
-import { applyMiddleware } from "redux";
+// import { applyMiddleware } from "redux";
 import logger from "redux-logger";
 
 import rootReducer from "./root-reducer";
 
 const middlewares = [logger];
 
-const middlewareEnhancer = applyMiddleware(...middlewares);
-
 const store = configureStore(
     { 
-        reducer: rootReducer, middlewareEnhancer,
+        reducer: rootReducer,
         middleware: (getDefaultMiddleware) => getDefaultMiddleware({
             immutableCheck: false,
             serializableCheck: false,
-            // {
-            // //     // Ignore these field paths in all actions
-            //     // ignoredActionPaths: ['meta.arg', 'User'],
-            //  },
-        })
+             }).concat(middlewares)
     }
 );
 
